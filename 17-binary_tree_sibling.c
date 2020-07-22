c2c969783a429c7aa9b56212f27f1089dc0356d9
tatiana orejuela zapata <tatianaorezap@gmail.com>
2020-07-22 17:20:57 +0000

    [Binary Trees Tasks - Task 17/18]

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: pointer to the node to find the sibling
+ * Return: pointer to the sibling node
+ * NULL if -> node is NULL | parent is NULL | node has no sibling
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+	if (node->parent->left && node->parent->right)
+	{
+		if (node == node->parent->left)
+			return (node->parent->right);
+		else
+			return (node->parent->left);
+	}
+	return (NULL);
+}
---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - finds the uncle of a node
+ * @node: pointer to the node to find the uncle
+ * Return: pointer to the uncle node
+ * NULL if -> node is NULL | parent is NULL | node has no uncle
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || !node->parent || !node->parent->parent)
+		return (NULL);
+	if (node->parent->parent->left && node->parent->parent->right)
+	{
+		if(node->parent == node->parent->parent->left)
+			return (node->parent->parent->right);
+		else
+			return (node->parent->parent->left);
+	}
+	return (NULL);
+}
