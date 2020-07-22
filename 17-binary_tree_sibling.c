475e2730e42431374005abf2e97117304d079f3b
tatiana orejuela zapata <tatianaorezap@gmail.com>
2020-07-22 18:22:15 +0000

    [Binary Trees Tasks - Refactor]

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,12 +10,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node || !node->parent)
 		return (NULL);
-	if (node->parent->left && node->parent->right)
-	{
-		if (node == node->parent->left)
-			return (node->parent->right);
-		else
-			return (node->parent->left);
-	}
-	return (NULL);
+	if (node == node->parent->left)
+		return (node->parent->right);
+	return (node->parent->left);
 }
--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,12 +10,8 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (!node || !node->parent || !node->parent->parent)
 		return (NULL);
-	if (node->parent->parent->left && node->parent->parent->right)
-	{
-		if(node->parent == node->parent->parent->left)
-			return (node->parent->parent->right);
-		else
-			return (node->parent->parent->left);
-	}
-	return (NULL);
+
+	if (node->parent == node->parent->parent->left)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
 }
